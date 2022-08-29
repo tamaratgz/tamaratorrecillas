6b016d306159272d0334b09ba03dc395a56139b2
Tamara Torrecillas <tamaratgz@gmail.com>
2022-08-29 18:14:24 +0200

    profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Check: http://localhost:3000/rails/info/routes
+	  root to: "pages#home"
+    # verb "path", to: "controller#action" >> http://localhost:3000/projects
+	  get "projects", to: "pages#projects", as: :projects
 end
